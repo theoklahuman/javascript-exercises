333619b1e1f8ec56b4ed2637f0ead9d9b18fa1d8
theoklahuman <theoklahuman@gmail.com>
2022-05-16 00:24:00 -0500

    Complete excercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arrays) {
+  {let num = 0;
+for (array of arrays) {
+       num += array;
+  }; return num;
+};return 0;
 };
 
-const multiply = function() {
-
+const multiply = function(arrays) {
+  {let num = 1;
+for (array of arrays) {
+       num *= array;
+  }; return num;
+};return 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+  if (a == 0) {
+  return 1;
+  } else {
+  let num = 1;
+  for (let i = 1; i <= a; i++) {
+  num *= i;
+  }; return num;
+  }
+  }
 
 // Do not edit below this line
 module.exports = {
